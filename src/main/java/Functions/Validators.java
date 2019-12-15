2b54f55f729e9d1d91ad6e2db67f4bcf1133b753
andre <andrey_zelikay@mail.ru>
2019-12-15 22:36:36 +0300

    refactor regex

--- src/main/java/Functions/Validators.java
+++ src/main/java/Functions/Validators.java
@@ -12,7 +12,7 @@ import java.util.regex.Pattern;
 public class Validators {
 
     public static boolean validateNumber(String number) throws IllegalArgumentException{
-        Pattern pattern = Pattern.compile("(((\\+?375|80)(29|33|44))[0-9]{7})|(80(17|21|22|23|16|15)[0-9]{7})");
+        Pattern pattern = Pattern.compile("(((\\+?375|80)(29|33|44))[0-9]{7}$)|(80(17|21|22|23|16|15)[0-9]{7}$)");
         Matcher matcher = pattern.matcher(number);
         return matcher.find();
     }
